ea8b7427af7732d23defc0a90fc1cd052172e415
return0927 <bjleh0927@gmail.com>
2018-03-10 23:39:25 +0900

    Restructed data-getting progress & Made the feature formatting string for output

--- kakao.py
+++ kakao.py
@@ -6,17 +6,29 @@ def keyboard():
     echo \'
         {
             "type": "buttons",
-            "buttons": []
+            "buttons": [%s]
         }\';
     ?>
     '''
-    print("\n버튼 설정하기\n")
-    count = input("추가할 버튼 개수를 입력하세요 >> ")
-    count = int(count)
-    button = [input("%s 번째 버튼이름 설정 >> " % (n+1)) for n in range(count)]
-    print("\n")
-
-    f = open("keyboard.php",'w')
+    print("\n    / 버튼 설정하기 /\n")
+    print(" 사용자가 채팅방에 들어왔을 때, \n    하단에 보여질 버튼의 문구를 입력해주세요.")
+    print(" * 끝내려면 빈칸으로 남겨두고 엔터를 쳐주세요.", end="\n\n")
+    
+    _Ubutts = []
+    
+    while True:
+    	inp = input("  > ")
+    	
+    	if inp: _Ubutts.append(inp)
+    	else: break
+    
+    print("{}개의 값을 입력받았습니다.".format(len(_Ubutts)))
+    print(", ".join(_Ubutts))
+    
+    base = base % ( ", ".join(_Ubutts) )
+    
+    f = open("keyboard.php",'w', encoding="UTF-8")\
+        .write(base)
 
 
 print("kakaotalk chatbot maker by python")
@@ -30,12 +42,3 @@ cho = input("선택 >> ")
 if cho == "1":
     keyboard()
 
-'''
-<?php
-echo '
-    {
-        "type": "buttons",
-        "buttons": ["사용법","급식"]
-    }';
-?>
-'''
